83d9349abce14d884411b7d193462d0c894f7d8a
griff <nicholas.s.griffen@gmail.com>
2018-08-19 22:48:28 -0600

    make party

---
+++ party.js
@@ -0,0 +1,12 @@
+let musicSrc = 'https://ia802808.us.archive.org/29/items/01.OneMoreTime/01.%20One%20More%20Time.ogg'
+let boomBox = `<audio autoplay><source src=${musicSrc} type="audio/ogg"></audio>`
+
+function loadPartyStyle() {
+  let style = $("#style")
+  style.attr('href', 'party.css')
+}
+
+function startParty() {
+  loadPartyStyle();
+  $('main').before(boomBox)
+}
