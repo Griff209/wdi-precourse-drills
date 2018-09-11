08afcdf638976dd31e60ce0b31307beb143c1f2d
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-09-10 21:09:08 -0600

    implement promises, remove autoplay, add controls

--- party.js
+++ party.js
@@ -1,28 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("party").addEventListener("click", startParty)
-})
-
-function startParty() {
-  loadPartyStyle()
+  
+  document.getElementById("startParty").addEventListener("click", () => {
+    loadPartyStyle()
     .then(loadBoomBox)
     .catch(error => document.querySelector('main').insertBefore(`<p>Error: ${error}</p>`))
-}
+  })
 
-function loadPartyStyle() {
-  return Promise.resolve(document.getElementById("style")
-    .setAttribute('href', 'party.css'))
-}
+  document.getElementById("stopParty").addEventListener("click", () => {
+    loadProStyle()
+    .then(loadBoomBox)
+    .then(unloadBoomBox)
+    .catch(error => document.querySelector('main').insertBefore(`<p>Error: ${error}</p>`))
+  })
 
-<<<<<<< HEAD
-function loadBoomBox() {
-  const musicSrc = 'https://ia802808.us.archive.org/29/items/01.OneMoreTime/01.%20One%20More%20Time.ogg'
-  const boomBox = `<audio autoplay><source src=${musicSrc} type="audio/ogg"></audio>`
-  document.querySelector('main').insertBefore(boomBox)
-} 
-
-=======
-function startParty() {
-  loadPartyStyle()
-  $('main').before(boomBox)
-}
->>>>>>> 84ab3080098544718ed0fc062d1462fe60907ea8
+  function loadPartyStyle() {
+    document.getElementById("style").setAttribute('href', 'party.css')
+      return Promise.resolve(true)
+  }
+  
+  function loadProStyle() {
+    document.getElementById("style").setAttribute('href', 'professional.css')
+      return Promise.resolve(true)
+  }
+  
+  function loadBoomBox() {
+    let box
+    if (!document.getElementById('boomBox')) {
+      box = document.createElement('div')
+      box.setAttribute('id', 'boomBox')
+      box.innerHTML += `<audio controls><source src="https://ia802808.us.archive.org/29/items/01.OneMoreTime/01.%20One%20More%20Time.ogg" type="audio/ogg"></audio>`
+      document.querySelector('main').appendChild(box)
+    } else {
+      box = document.getElementById('boomBox')
+    } 
+    return Promise.resolve(box)
+  } 
+  
+  function unloadBoomBox(boomBox) {
+    document.querySelector('main').removeChild(boomBox)
+  }
+})
